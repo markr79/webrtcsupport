cfcb906d0a578c16cb20cdb54d77e86b232c326f
markr79 <mrubenchik@gmail.com>
2015-04-27 11:36:45 -0400

    Add nodejs support

--- index-node.js
+++ index-node.js
@@ -1,20 +1,38 @@
 // Once node.js gains a robust module integrating WebRTC that can
 // be easily added to dependencies, we'll update this to expose it
+var webrtc = require('wrtc');
 
+var prefix = 'nodejs';
+var version = process.version;
+
+var PC = webrtc.mozRTCPeerConnection || webrtc.webkitRTCPeerConnection || webrtc.RTCPeerConnection;
+var IceCandidate = webrtc.mozRTCIceCandidate || webrtc.RTCIceCandidate;
+var SessionDescription = webrtc.mozRTCSessionDescription || webrtc.RTCSessionDescription;
+var MediaStream = webrtc.webkitMediaStream || webrtc.MediaStream;
+var screenSharing = false;
+var AudioContext = webrtc.AudioContext || webrtc.webkitAudioContext;
+var supportVp8 = true;
+var getUserMedia;
+
+
+// export support flags and constructors.prototype && PC
 module.exports = {
-    support: false,
-    supportRTCPeerConnection: false,
-    supportVp8: false,
-    supportGetUserMedia: false,
-    supportDataChannel: false,
-    supportWebAudio: false,
-    supportMediaStream: false,
-    supportScreenSharing: false,
-    prefix: undefined,
-    AudioContext: undefined,
-    PeerConnection: undefined,
-    SessionDescription: undefined,
-    IceCandidate: undefined,
-    MediaStream: undefined,
-    getUserMedia: undefined
+    prefix: prefix,
+    browserVersion: version,
+    support: !!PC && supportVp8,
+    // new support style
+    supportRTCPeerConnection: !!PC,
+    supportVp8: supportVp8,
+    supportGetUserMedia: !!getUserMedia,
+    supportDataChannel: !!PC,
+    supportWebAudio: !!AudioContext,
+    supportMediaStream: !!MediaStream,
+    supportScreenSharing: !!screenSharing,
+    // constructors
+    AudioContext: AudioContext,
+    PeerConnection: PC,
+    SessionDescription: SessionDescription,
+    IceCandidate: IceCandidate,
+    MediaStream: MediaStream,
+    getUserMedia: getUserMedia
 };
